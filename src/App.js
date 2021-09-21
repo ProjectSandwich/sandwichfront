24b7c4368e2a7c021c79b9b28eb04645d875d128
mistidinzy <mistidinzy@gmail.com>
2021-09-21 12:16:39 -0500

    created mapPage component to render map class onto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import Map from './components/map'
 
 function App() {
   return (
     <div className="App">
-      <Map/>
     </div>
   );
 }
---
+++ src/components/mapPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import GoogleMapReact from 'google-map-react';
+import Map from './components/map'
+
+export default class mapPage extends React.Component {
+  return() {
+    <Map/>
+  }
+}
