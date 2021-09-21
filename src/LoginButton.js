d17ecc9c75632d1d27aaf332ba99d3ca23ff5cec
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-21 12:07:37 -0500

    login button is now working with auth0, add bootstrap button with secondary variant

--- src/LoginButton.js
+++ src/LoginButton.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'react-bootstrap';
 
 const LoginButton = () => {
   const auth0 = useAuth0();
@@ -8,7 +9,7 @@ const LoginButton = () => {
 
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return <Button variant="secondary" onClick={() => loginWithRedirect()}>Log In</Button>;
 };
 
 export default LoginButton;
