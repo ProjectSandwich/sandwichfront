785ab5ac3b2c1ca540e8925bbe0f188f20cd6778
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-21 12:29:04 -0500

    headerunstyled

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+import './header.css';
+import { Container } from "react-bootstrap";
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      
+  <Navbar bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">PROJECT SANDWICH</Navbar.Brand>
+     {/* <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#Map">SANDWICH FINDER</Nav.Link>
+      <<Nav className="nav-Link">
+     Nav.Link href="#pricing">About Us</Nav.Link>
+    </Nav> */}
+    </Container>
+    </Navbar>
+    )
+  }
+}
+export default Header;
