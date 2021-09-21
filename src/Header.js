ab2fbec7ab24fb4bbf1f68134a4cb9fdabf46509
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-21 15:52:45 -0500

    footer without styling

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
-import React from "react";
-import { Navbar } from "react-bootstrap";
-//  import { Link } from "react-router-dom";
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './header.css';
 import { Container } from "react-bootstrap";
 
@@ -8,18 +8,21 @@ import { Container } from "react-bootstrap";
 class Header extends React.Component {
   render() {
     return (
-      
+  <>    
   <Navbar bg="dark" variant="dark">
     <Container>
     <Navbar.Brand href="#home">PROJECT SANDWICH</Navbar.Brand>
-     {/* <Nav.Link href="#home">Home</Nav.Link>
+    <Nav className="nav-Link">
+     <Nav.Link href="#home">Home</Nav.Link>
       <Nav.Link href="#Map">SANDWICH FINDER</Nav.Link>
-      <<Nav className="nav-Link">
-     Nav.Link href="#About Us">About Us</Nav.Link> */}
-    {/* </Nav> */}
+      <Nav.Link href="#Profile">Profile</Nav.Link>
+      <Nav.Link href="#About Us">About Us</Nav.Link> 
+    </Nav>
     </Container>
-    </Navbar>
+  </Navbar>
+  </>
     )
   }
 }
 export default Header;
+
