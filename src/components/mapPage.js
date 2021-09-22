027ec02ff9357a5a70c153652774a7f2a07a47ae
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-22 11:52:46 -0500

    create post/axios/async to save a new sandwich to database on click of the button

--- src/components/mapPage.js
+++ src/components/mapPage.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import CreateSandwich from './CreateSandwich';
+import axios from 'axios';
 // import GoogleMapReact from 'google-map-react';
 import Map from './map';
+const SERVER = process.env.REACT_APP_SERVER;
 
 export default class MapPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sandwiches: null,
+    };
+  }
 
-/* We need to make a handleSave = async to be able to save our new sandwich to our database from our form */
+  handleCreate = async sandwichInfo => {
+    let apiURL = `${SERVER}/getSandwiches`;
+    let results = await axios.post(apiURL, sandwichInfo);
+    let newSandwich = results.data;
+    console.log(newSandwich);
+
+    this.fetchSandwiches();
+  }
 
   render() {
     return (
       <>
         <Map />
-        <CreateSandwich onSave={this.handleSave} />
+        <CreateSandwich onSave={this.handleCreate} />
       </>
     )
   }
-};
+}
