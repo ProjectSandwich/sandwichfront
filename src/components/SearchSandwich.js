204a0fa4cf54103513d1e93bb5cbbb97f9fd5bf2
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-22 15:37:42 -0500

    create searchsandwich component to add yelp capability

---
+++ src/components/SearchSandwich.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+const key = process.env.YELP_API_KEY
+const yelpURL = 'https://api.yelp.com/v3/businesses/search';
+
+export default class SearchSandwich extends React.Component {
+  render() {
+    return (
+      <>
+      
+      </>
+    )
+  }
+}
