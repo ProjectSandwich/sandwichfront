ab8740ff4fa13725d5609245fa1d880aff2ca9d0
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-22 20:40:44 -0500

    start get for new sandwich search

--- src/components/SearchSandwich.js
+++ src/components/SearchSandwich.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 
+const apiUrl = process.env.REACT_APP_SERVER
+
 export default class SearchSandwich extends React.Component {
-  render() {
-    return (
-      <>
-      
-      </>
-    )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showModal: false,
+    };
   }
-}
+
+  handleSearch = async event => {
+    event.preventDefault();
+
+    let elements = event.target.default;
+    let formData = {
+      sandwich: 
+      restaurant:
+    }
+  }
+
+    getYelpData =
+
+      render() {
+      return (
+        <>
+
+        </>
+      )
+    }
+  }
