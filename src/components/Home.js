209f24ab36cd4721ed4892dbf46b6d1b934bc2ed
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-23 17:57:17 -0500

    clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,8 +20,8 @@ export class Home extends React.Component {
             <Card.Body>
               <h1>Welcome to Project Sandwich!</h1>
               <h2>Have you have ever had a sandwich that was so good, you could say it was a transcendental experience? 
-You just can’t stop thinking about it and want to share it with others, and maybe even find more sandwiches in hopes of replicating that experience.
-PROJECT SANDWICH is a site for avid sandwich aficionados to come together and rave about all the cathartic Cheesesteaks, transformative Tunamelts and profound PB&Js of the world. Join us in Club Sandwich and find your next best sandwich experience today!
+                  You just can’t stop thinking about it and want to share it with others, and maybe even find more sandwiches in hopes of replicating that experience.
+                  PROJECT SANDWICH is a site for avid sandwich aficionados to come together and rave about all the cathartic Cheesesteaks, transformative Tunamelts and profound PB&Js of the world. Join us in Club Sandwich and find your next best sandwich experience today!
               </h2>
             </Card.Body>
           </Card>
