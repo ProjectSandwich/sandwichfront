a0ddf55141562a18fcbeff4f47e018f4e7ae00f0
Ian Whitmore <ianwhitmor@gmail.com>
2021-09-23 11:26:29 -0500

    made results from search render in a card with restaurant name and clickable link to yelp page for specific restaurant

--- src/components/SearchLocation.js
+++ src/components/SearchLocation.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 export default class SearchLocation extends React.Component {
   render() {
     const location = this.props.location
 
     return (
-      <div>
-        {location.restaurant}
-      </div>
+      <>
+        <Card style={{ width: '10rem' }}>
+          <Card.Body>
+            <Card.Title>{location.restaurant}</Card.Title>
+            <Card.Text><a href={location.yelpUrl}>Link</a></Card.Text>
+          </Card.Body>
+        </Card>
+      </>
     )
   }
 }
