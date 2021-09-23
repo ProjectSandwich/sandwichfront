ce4f0efa5a9ae860ffac059ac270b64589d2ef3b
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-23 13:38:49 -0500

    improved navbar

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 import './header.css';
 import { Link } from 'react-router-dom';
 
@@ -14,13 +15,12 @@ class Header extends React.Component {
   <Navbar style={{backgroundColor: "#58595b", position: "sticky"}} variant="dark" fixed="top">
     
   <Navbar.Brand href="/">PROJECT SANDWICH</Navbar.Brand>
-    <Nav className="nav-Link">
+    <Nav>
      <Nav.Link as={Link} to="/">Home</Nav.Link>
      <Nav.Link as={Link} to="/mapPage">SANDWICH FINDER</Nav.Link>
      <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
-     <Nav.Link as={Link} to="/AboutUs">About Us</Nav.Link> 
+     <Nav.Link as={Link} to="/AboutUs">About Us</Nav.Link>
     </Nav>
-    
   </Navbar>
   </>
     )
